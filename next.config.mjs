56ae715cf20ce8a23f819984d31e495944b40f04
tengkuhafidz <tengkuhafidz@gmail.com>
2024-04-21 11:01:08 +0800

    fix(config): temp fix w useSearchParams

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        missingSuspenseWithCSRBailout: false, // FIXME: https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
+    },
+};
 
 export default nextConfig;
